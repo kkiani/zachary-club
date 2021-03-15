b64907330d9e6e5a51f4a1ef0781062950aa20ad
Kiarash Kiani <hi@kiarash.info>
2021-03-15 13:47:38 +0330

    merge model to entry file

--- app.py
+++ app.py
@@ -2,11 +2,27 @@ import os, pickle, argparse
 from torch.autograd import grad
 import dgl
 import torch as th
+import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
 import numpy as np
+from dgl.nn import GraphConv
 
-from model.graph_neural_network import GraphConvolutionNetwork
+
+class GraphConvolutionNetwork(nn.Module):
+    def __init__(self, in_feats, hidden_size, num_classes):
+        super(GraphConvolutionNetwork, self).__init__()
+
+        self.gcl1 = GraphConv(in_feats, hidden_size)
+        self.gcl2 = GraphConv(hidden_size, num_classes)
+
+    def forward(self, graph: dgl.DGLGraph, inputs):
+        x = self.gcl1(graph, input)
+        x = th.relu(x)
+        x = self.gcl2(graph, x)
+        
+        return th.softmax(x)
+
 
 
 def main():
