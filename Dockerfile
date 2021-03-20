ab947ce0511cd0ff07501c06349bc22bcc752a83
Kiarash Kiani <hi@kiarash.info>
2021-03-20 16:47:41 +0330

    init serve script

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM pytorch/pytorch:latest
 
-RUN pip install --no-cache dgl numpy
+RUN pip install --no-cache dgl numpy fastapi uvicorn[standard]
 
 
 COPY train.py /usr/bin/train
 COPY model /usr/bin/model
-# COPY serve.py /usr/bin/serve
+COPY serve.py /usr/bin/serve
 
 RUN chmod 755 /usr/bin/train
-# RUN chmod 755 /usr/bin/serve
+RUN chmod 755 /usr/bin/serve
 
-# EXPOSE 8080
+EXPOSE 8080
--- serve.py
+++ serve.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python
 
+import os
+from pandas.core.algorithms import mode
+from starlette.routing import Host
+import uvicorn
+from fastapi import FastAPI, Response
+from io import StringIO
+import pandas as pd
+import torch as th
+
+
+app = FastAPI()
+model_dir = '/opt/ml/model'
+model = th.load(os.path.join(model_dir, 'karate_club.pt'))
+
+
+app.get('/ping')
+def ping():
+    return Response(content='\n', status_code=200)
+
+app.post('/invocations')
+def predict(data: bytes):
+    data_decoded = data.decode('utf-8')
+    data_stream = StringIO(data_decoded)
+    df = pd.read_csv(data_stream, header=None)
+    response = model.predict(df)
+
+    return Response(content=str(response), status_code=200)
+
+
 def main():
-    raise NotImplementedError
+    uvicorn.run(app, port=8080, host='0.0.0.0')
 
 if __name__ == '__main__':
     main()
